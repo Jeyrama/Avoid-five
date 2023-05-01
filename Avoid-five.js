3bcf90eb6b1ba40463c0b6bb4b1c66f3cb63dd38
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-05-01 09:53:10 -0700

    Create Avoid-five.js

---
+++ Avoid-five.js
@@ -0,0 +1,26 @@
+/*
+
+*/
+
+
+// Solution
+
+function dontGiveMeFive(start, end) {
+  let count = 0
+  for (let i = start; i <= end; i++) {
+    if (!/5/.test(i)) {
+      count++
+    }
+  }
+  return count
+}
+
+// or
+
+function dontGiveMeFive(start, end) {
+  let res = [];
+  for (let i = start; i <= end; i++) {
+    if (!i.toString().includes('5')) res.push(i);
+  }
+  return res.length;
+}
